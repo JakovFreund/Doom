9c2b42d25dfd861955329e11a956e23d85dcb3aa
JakovFreund <jakovfreund@gmail.com>
2017-04-27 05:15:07 -0200

    Add files via upload

---
+++ varchar.js
@@ -0,0 +1,12 @@
+var a = document.getElementById("abc");
+var b = document.getElementById("def");
+a.innerHTML=b.innerHTML;
+
+function oluja(){
+document.getElementById("cet").src=document.getElementById("u").src;
+}
+
+function boja(){
+    document.getElementById("def").style.backgroundColor="blue";
+    document.getElementById("abc").style.backgroundColor="red"
+}
---
+++ varchar2.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Ucenik on 27.4.2017..
+ */
+function sakrij() {
+    document.getElementById("a").style.display = "none";
+}
+function prikazi() {
+    document.getElementById("a").style.display = "block";
+}
